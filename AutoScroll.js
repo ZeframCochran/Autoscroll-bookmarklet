aec7b3a7ff897ffb7b4737bad443ae93079dae82
ZeframCochran <Anonygram@gmail.com>
2015-01-02 23:31:30 -0800

    pausing now works. "trigger the bookmarklet again to stop scrolling"

--- AutoScroll.js
+++ AutoScroll.js
@@ -1,2 +1,2 @@
-//Speed is higher when the delay is lower.
-javascript:stop=false;(function recursion(){if(!stop){setTimeout(function(){window.scrollBy(0,1); recursion(); },300);}})();
+//Speed is higher when the delay is lower, trigger the bookmarklet again to stop scrolling
+avascript:if (typeof stop !== 'undefined') {stop=!stop;}else{stop=false;}(function recursion(){if(!stop){setTimeout(function(){window.scrollBy(0,1); recursion(); },300);}})();
