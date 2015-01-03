7e7fa60c16c77df5fed4dd39505d268acf318f63
ZeframCochran <Anonygram@gmail.com>
2015-01-03 00:22:58 -0800

    Added delay variable for increased clarity.

--- AutoScroll.js
+++ AutoScroll.js
@@ -1,2 +1,2 @@
 //Speed is higher when the delay is lower, trigger the bookmarklet again to stop scrolling
-javascript:if ("stop" in window) {stop=!stop;}else{stop=false;}(function recursion(){if(!stop){setTimeout(function(){window.scrollBy(0,1); recursion(); },300);}})();
+javascript:if ("stop" in window) {stop=!stop;}else{stop=false;}(function recursion(){if(!stop){setTimeout(function(){window.scrollBy(0,1); recursion(); },delay=300);}})();
