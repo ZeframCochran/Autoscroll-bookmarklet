d80fe6d12500f45242a04ce7f37ae945bb746f15
ZeframCochran <Anonygram@gmail.com>
2015-02-28 22:37:21 -0800

    Wrapped bookmarklet in anonymous function
    This works on firefox woo!
    Now to re-try the tests..

--- AutoScroll.js
+++ AutoScroll.js
@@ -1,6 +1,5 @@
 //Speed is higher when the delay is lower, trigger the bookmarklet again to stop scrolling
-javascript:if ("intervalNumber" in window) { clearInterval(window.intervalNumber); delete window.intervalNumber; }else{ begin = (function begin(){ window.intervalNumber = setInterval(function(){ window.scrollBy(0,1); },delay=300); }); begin(); }
-
+(function(){if ("intervalNumber" in window) {trash = clearInterval(window.intervalNumber);trash = delete window.intervalNumber;}else{begin = (function begin(){window.intervalNumber = setInterval(function(){window.scrollBy(0,1);},delay=300);});begin();}})();
 
 Pretty version:
 
