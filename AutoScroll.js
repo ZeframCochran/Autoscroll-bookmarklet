1f553b4cb81b765820cd6ab927c8dfe22ce8ce14
ZeframCochran <Anonygram@gmail.com>
2015-02-28 21:57:28 -0800

    Replace recursion with setInterval
    Now I need to test the bookmarklet on all my devices again, and remind myself to test it on a macbook when I get the chance.

--- AutoScroll.js
+++ AutoScroll.js
@@ -1,2 +1,17 @@
 //Speed is higher when the delay is lower, trigger the bookmarklet again to stop scrolling
-javascript:if ("stop" in window) {stop=!stop;}else{stop=false;}(function recursion(){if(!stop){setTimeout(function(){window.scrollBy(0,1); recursion(); },delay=300);}})();
+avascript:if ("intervalNumber" in window) { clearInterval(window.intervalNumber); delete window.intervalNumber; }else{ begin = (function begin(){ window.intervalNumber = setInterval(function(){ window.scrollBy(0,1); },delay=300); }); begin(); }
+
+
+Pretty version:
+
+if ("intervalNumber" in window) {
+	clearInterval(window.intervalNumber);
+	delete window.intervalNumber;
+}else{
+	begin = (function begin(){
+			window.intervalNumber = setInterval(function(){
+				window.scrollBy(0,1);
+			},delay=300);
+	});
+	begin();
+}
