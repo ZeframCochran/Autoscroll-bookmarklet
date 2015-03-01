22f004ad693256eba31db7e92f32bbca70e84fed
ZeframCochran <Anonygram@gmail.com>
2015-02-28 22:25:33 -0800

    Testing update
    This still works in chrome on my pc and tablet, but it causes annoying redirects in firefox and does not do anything on my iphone (in chrome or safari)
    
    I might be able to make it work in firefox if I find some way to end the interval without returning a value.

--- AutoScroll.js
+++ AutoScroll.js
@@ -1,5 +1,5 @@
 //Speed is higher when the delay is lower, trigger the bookmarklet again to stop scrolling
-avascript:if ("intervalNumber" in window) { clearInterval(window.intervalNumber); delete window.intervalNumber; }else{ begin = (function begin(){ window.intervalNumber = setInterval(function(){ window.scrollBy(0,1); },delay=300); }); begin(); }
+javascript:if ("intervalNumber" in window) { clearInterval(window.intervalNumber); delete window.intervalNumber; }else{ begin = (function begin(){ window.intervalNumber = setInterval(function(){ window.scrollBy(0,1); },delay=300); }); begin(); }
 
 
 Pretty version:
